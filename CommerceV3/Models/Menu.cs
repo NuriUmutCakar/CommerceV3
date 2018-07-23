15f0ae74b573d8c08815b4b32b6cae182079ef41
Umut ÇAKAR <nuriumut.cakar@hotmail.com>
2018-07-23 17:47:42 +0300

    to be continued (23.07.2018)

--- CommerceV3/Models/Menu.cs
+++ CommerceV3/Models/Menu.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace CommerceV3.Models
     public class Menu
     {
 		public String Id { get; set; }
+		[Display(Name="Ad: ")]
 		public String Name { get; set; }
 		public virtual ICollection<MenuItem> MenuItems { get; set; }
 	}
--- CommerceV3/Models/Region.cs
+++ CommerceV3/Models/Region.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,9 +10,12 @@ namespace CommerceV3.Models
     public class Region
     {
 		public string Id { get; set; }
+		[Display(Name="Ad: ")]
 		public string Name { get; set; }
 		public RegionType RegionType { get; set; }
+		[Display(Name = "Ad: ")]
 		public string ParentRegionId { get; set; }
+		[Display(Name = "Ana Bölge: ")]
 		[ForeignKey("ParentRegionId")]
 		public Region ParentRegion { get; set; }
 
